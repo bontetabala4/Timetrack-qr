a9babe270174d60d6d542a21c23a9333a0044138
bontetabala4 <bontetabala4@gmail.com>
2026-04-18 23:52:56 +0100

    fix user frontend imports

--- user-frontend/src/assets/main.tsx
+++ user-frontend/src/assets/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/router'
-import { AuthProvider } from './context/AuthContext'
-import { ToastProvider } from './context/ToastContext'
+import { router } from '../routes/router'
+import { AuthProvider } from '../context/AuthContext'
+import { ToastProvider } from '../context/ToastContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
